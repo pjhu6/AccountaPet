0baf7dcbf57c301012a22bfdcab3afccfc5fdac4
Wilmerlin01 <39103309+Wilmerlin01@users.noreply.github.com>
2023-02-21 22:01:03 -0800

    Initial framework for web app

---
+++ webapp.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/shop')
+def shop():
+    return render_template('shop.html')
+
+@app.route('/pet_status')
+def pet_status():
+    return render_template('pet_status.html')
+
+@app.route('/settings')
+def settings():
+    return render_template('settings.html')
+
+if __name__ == '__main__':
+    app.run()
